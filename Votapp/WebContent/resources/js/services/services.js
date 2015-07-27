3d732b7ec719b0c8992930ef8fb83aaf6b129145
Cristian <CJ@CJ-PC>
2015-07-27 04:24:09 -0300

    Bug fix + apiEndPoint Votapp

--- Votapp/WebContent/resources/js/services/services.js
+++ Votapp/WebContent/resources/js/services/services.js
@@ -4,10 +4,10 @@
 // In this case it is a simple value service.
 angular.module('app.services', []).
 value('version', '0.1')
-.factory('ConsultoraFactory',['$http', function($http) {
+.factory('ConsultoraFactory',['$http','ApiEndpointFactory', function($http, ApiEndpointFactory) {
 	return{
 		getConsultora:function(idConsultora){
-			return $http.get('http://localhost:8080/Votapp/services/consultoras/'+idConsultora)
+			return $http.get(ApiEndpointFactory.ApiEndpoint +'/Votapp/services/consultoras/'+idConsultora)
 		}
 	}
 	
@@ -18,9 +18,9 @@ value('version', '0.1')
 		
 	}
 }])
-.factory('EleccionFactory', ['$http',function($http) {
+.factory('EleccionFactory', ['$http','ApiEndpointFactory',function($http, ApiEndpointFactory) {
 	var elecciones = null;
-    var promise = $http.get('http://localhost:8080/Votapp/services/eleccion/getElecciones')
+    var promise = $http.get(ApiEndpointFactory.ApiEndpoint +'/Votapp/services/eleccion/getElecciones')
     .success(function (data) {
     	elecciones = data;
     });
@@ -31,7 +31,17 @@ value('version', '0.1')
           return elecciones;//.getSomeData();
       },
       getConsultora:function(idEleccion){
-    	  return $http.get('http://localhost:8080/Votapp/services/eleccion/'+idEleccion)
+    	  return $http.get(ApiEndpointFactory.ApiEndpoint +'/Votapp/services/eleccion/'+idEleccion)
       }
     }
+}])
+
+.factory('ApiEndpointFactory', ['$http','$location', function($http, $location) {
+	
+	var ApiEndpoint = $location.protocol() + "://" + $location.host() + ":" + $location.port();
+	
+	return{
+		ApiEndpoint : ApiEndpoint
+	}	
+	
 }])
